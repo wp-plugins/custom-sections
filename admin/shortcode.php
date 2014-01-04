bc69ee5808e6bcba0c842c020a25c9465c707b0e
jvwissen <jvwissen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-04 16:50:42 +0000

    tagging version 0.4.2
    git-svn-id: https://plugins.svn.wordpress.org/custom-sections/tags/0.4.2@832927 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/shortcode.php
+++ admin/shortcode.php
@@ -5,11 +5,11 @@
 	$name  = $section->post_name;
 ?>
 <div id="section-shortcode-display">
-	<p>Use the shortcode to use this section in another post, or use the php function to use it directly in your theme. Change 'basic' to any other custom created section-&lt;template&gt;.php template file in your theme folder.</p>
+	<p>Use the shortcode to use this custom section in another post, or use the php function call to use it directly in your theme. Change 'default' to any other custom created section-&lt;template&gt;.php template file in your theme folder.</p>
 	<div class="section-shortcode">
-		<span id="ection-shortcode"><pre>[section name="<?php echo $name; ?>" template="basic"]</pre></span>
+		<span id="ection-shortcode"><pre>[section id="<?php echo $pid; ?>" template="default"]</pre></span>
 	</div>
 	<div class="section-function">
-		<span id="section-function"><pre>&lt;?php show_section('<?php echo $name; ?>', array('template' => 'basic')); ?&gt;</pre></span>
+		<span id="section-function"><pre>&lt;?php show_section('<?php echo $pid; ?>', array('template' => 'default')); ?&gt;</pre></span>
 	</div>
 </div>
